1c073ac832ac7e1f3166d79230a96fd5386e973c
Masayuki Onishi <omccello@gmail.com>
2021-04-08 23:13:00 +0900

    themeに型定義追加

--- src/_theme.ts
+++ src/_theme.ts
@@ -1,7 +1,8 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import { blue, indigo } from "@material-ui/core/colors";
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
 
-export const theme = createMuiTheme({
+export const theme: Theme = createMuiTheme({
   palette: {
     primary: {
       main: indigo[700],
