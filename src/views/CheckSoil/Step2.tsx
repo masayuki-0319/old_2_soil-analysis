85d384b1b661fbd37b4a830a22299cfe67516ee3
Masayuki Onishi <omccello@gmail.com>
2021-04-12 22:59:45 +0900

    Step2,Step3 のモック作成

---
+++ src/views/CheckSoil/Step2.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Step2 = () => {
+  return <>Step2 画面</>;
+};
+
+export default Step2;
---
+++ src/views/CheckSoil/Step3.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Step3 = () => {
+  return <>Step3 画面</>;
+};
+
+export default Step3;
